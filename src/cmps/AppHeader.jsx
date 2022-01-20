a72eae2d72e0ca47f11acfdf625943b1943f55f2
Oren-Papismedov <orenpapi11@gmail.com>
2022-01-20 11:25:24 +0200

    app header

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { SiAirbnb } from 'react-icons/si';
 import { FaUserCircle } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi"
 
 function Header() {
     return (
-        <div>
+        <div className='header'>
 
             <div className='header-left'>
                 <SiAirbnb size="2em" color="blue" />
-                <span> homey</span>
+                <span className='homey'>  homey</span>
             </div>
 
             <div className='header-right'>
-                <p>Became a Host</p>
-                <p>Explore</p>
-                <FaUserCircle size="2em" color="#717171"/>
+                <button>Became a Host</button>
+                <button>Explore</button>
+                <button className='user-icon'><GiHamburgerMenu className='ham-icon' size="1.05rem"/><FaUserCircle size="1.5rem" color="#717171"/></button>
+                
             </div>
 
 

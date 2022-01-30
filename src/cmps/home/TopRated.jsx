6d63fe5a9b9e515cc7cbb99a3ada83ae22a97160
eylon frisch <you@example.com>
2022-01-30 19:10:37 +0200

    commit

--- src/cmps/home/TopRated.jsx
+++ src/cmps/home/TopRated.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { loadStays } from '../../store/stay.action.js'
-import {StayPreview} from '../StayPreview.jsx'
+import { StayPreview } from '../StayPreview.jsx'
 import { reviewService } from '../../services/review.service.js'
 
 class _TopRated extends React.Component {
@@ -15,14 +15,14 @@ class _TopRated extends React.Component {
         this.getTopRatedStays()
     }
 
-    getTopRatedStays = async() => {
-        await this.props.loadStays() 
-            let {stays} = this.props
-            stays.sort( (a,b) => {
-                return a.reviews.length - b.reviews.length
-            })
-            stays = stays.slice(0, 4)
-            this.setState({ topRatedStays: stays })
+    getTopRatedStays = async () => {
+        let stays =  await this.props.loadStays()   
+        // let { stays } = this.props
+        stays.sort((a, b) => {
+            return a.reviews.length - b.reviews.length
+        })
+        stays = stays.slice(0, 4)
+        this.setState({ topRatedStays: stays })
     }
 
 
@@ -37,24 +37,24 @@ class _TopRated extends React.Component {
 
                 <h1>Top Rated <span>homies</span></h1>
 
-                <section className= 'stay-list card-grid'>
+                <section className='stay-list card-grid'>
 
                     {
                         topRatedStays.map((stay, index) =>
                             // <section className="stay-list card-grid">
-                                <StayPreview key={stay._id} stay={stay} />)
-                            // </section>
-                    // <Link key={index} to={`/explore?location=${stay.loc.country}&minPrice=-Infinity&maxPrice=Infinity`}  className="top-rated-card clean-link">
-                    // <img src={stay.imgUrls[0]} alt="TopRated" />
-                    // <div className="city-details-container">
-                    //     <h3>{stay.loc.city}</h3>
-                    //     <div className="city-details">
-                    //     <h4>{stay.loc.country}</h4>
-                    //     </div>
-                    //     </div>
-                    //     </Link>
-                    // )
-               }
+                            <StayPreview key={stay._id} stay={stay} />)
+                        // </section>
+                        // <Link key={index} to={`/explore?location=${stay.loc.country}&minPrice=-Infinity&maxPrice=Infinity`}  className="top-rated-card clean-link">
+                        // <img src={stay.imgUrls[0]} alt="TopRated" />
+                        // <div className="city-details-container">
+                        //     <h3>{stay.loc.city}</h3>
+                        //     <div className="city-details">
+                        //     <h4>{stay.loc.country}</h4>
+                        //     </div>
+                        //     </div>
+                        //     </Link>
+                        // )
+                    }
 
                     {/* <Link to={`/explore?location=Bangkok&minPrice=-Infinity&maxPrice=Infinity`}  className="top-cities-card clean-link">
                 <img src="https://res.cloudinary.com/dxdtpxsax/image/upload/v1642760367/airbnb/pop-des/uwlmwbvado6u2qhqtky8.jpg" alt="TopCities" />
